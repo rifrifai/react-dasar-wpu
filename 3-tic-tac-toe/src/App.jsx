0b9279180cb4ba59b031151ac6442b0fb6577a1a
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-05-20 11:12:03 +0700

    handlerClick all

--- 3-tic-tac-toe/src/App.jsx
+++ 3-tic-tac-toe/src/App.jsx
@@ -11,17 +11,23 @@ function Square({ value, onSquareClick }) {
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  function handlerClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X"; // ['X', null, null, null]
+    setSquares(nextSquares);
+  }
+
   return (
     <div className="board">
-      <Square value={squares[0]} />
-      <Square value={squares[1]} />
-      <Square value={squares[2]} />
-      <Square value={squares[3]} />
-      <Square value={squares[4]} />
-      <Square value={squares[5]} />
-      <Square value={squares[6]} />
-      <Square value={squares[7]} />
-      <Square value={squares[8]} />
+      <Square value={squares[0]} onSquareClick={() => handlerClick(0)} />
+      <Square value={squares[1]} onSquareClick={() => handlerClick(1)} />
+      <Square value={squares[2]} onSquareClick={() => handlerClick(2)} />
+      <Square value={squares[3]} onSquareClick={() => handlerClick(3)} />
+      <Square value={squares[4]} onSquareClick={() => handlerClick(4)} />
+      <Square value={squares[5]} onSquareClick={() => handlerClick(5)} />
+      <Square value={squares[6]} onSquareClick={() => handlerClick(6)} />
+      <Square value={squares[7]} onSquareClick={() => handlerClick(7)} />
+      <Square value={squares[8]} onSquareClick={() => handlerClick(8)} />
     </div>
   );
 }
