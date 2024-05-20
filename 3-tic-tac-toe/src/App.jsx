f9ff2ca3ee1ce3266643d129718c1bfaa4fd9cd1
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-05-20 10:39:52 +0700

    handleClick X

--- 3-tic-tac-toe/src/App.jsx
+++ 3-tic-tac-toe/src/App.jsx
@@ -1,7 +1,17 @@
 import { useState } from "react";
 
 function Square() {
-  return <button className="square">X</button>;
+  const [value, setValue] = useState("");
+
+  function handleClick() {
+    setValue("X");
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
