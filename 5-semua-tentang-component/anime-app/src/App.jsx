ae037caa171a92fc679b342b7ca4fca417dec1fc
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-05-27 08:45:47 +0700

    Spread Components

--- 5-semua-tentang-component/anime-app/src/App.jsx
+++ 5-semua-tentang-component/anime-app/src/App.jsx
@@ -96,8 +96,6 @@ function NumResult() {
 function Main() {
   const [animes, setAnimes] = useState(animesData);
   const [selectedAnime, setSelectedAnime] = useState(animes[0]);
-  const [isOpen1, setIsOpen1] = useState(true);
-  const [isOpen2, setIsOpen2] = useState(true);
 
   function handleSelectedAnime(id) {
     const newAnime = animes.filter((anime) => anime.mal_id === id);
@@ -106,61 +104,77 @@ function Main() {
 
   return (
     <main className="main">
-      <div className="box">
-        <button
-          className="btn-toggle"
-          onClick={() => setIsOpen1((open) => !open)}
-        >
-          {isOpen1 ? "–" : "+"}
-        </button>
-        {isOpen1 && (
-          <ul className="list list-anime">
-            {animes?.map((anime) => (
-              <li
-                key={anime.mal_id}
-                onClick={() => handleSelectedAnime(anime.mal_id)}
-              >
-                <img src={anime.image} alt={`${anime.title} cover`} />
-                <h3>{anime.title}</h3>
-                <div>
-                  <p>
-                    <span>{anime.year}</span>
-                  </p>
-                </div>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-      <div className="box">
-        <button
-          className="btn-toggle"
-          onClick={() => setIsOpen2((open) => !open)}
-        >
-          {isOpen2 ? "–" : "+"}
-        </button>
-        {isOpen2 && (
-          <div className="details">
-            <header>
-              <img
-                src={selectedAnime.image}
-                alt={`${selectedAnime.title} cover`}
-              />
-              <div className="details-overview">
-                <h2>{selectedAnime.title}</h2>
+      <ListBox animes={animes} onSelectedAnime={handleSelectedAnime} />
+      <SelectedBox selectedAnime={selectedAnime} />
+    </main>
+  );
+}
+
+function ListBox({ animes, onSelectedAnime }) {
+  const [isOpen1, setIsOpen1] = useState(true);
+
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen1((open) => !open)}
+      >
+        {isOpen1 ? "–" : "+"}
+      </button>
+      {isOpen1 && (
+        <ul className="list list-anime">
+          {animes?.map((anime) => (
+            <li
+              key={anime.mal_id}
+              onClick={() => onSelectedAnime(anime.mal_id)}
+            >
+              <img src={anime.image} alt={`${anime.title} cover`} />
+              <h3>{anime.title}</h3>
+              <div>
                 <p>
-                  {selectedAnime.year} &bull; {selectedAnime.score}
+                  <span>{anime.year}</span>
                 </p>
               </div>
-            </header>
-            <section>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+function SelectedBox({ selectedAnime }) {
+  const [isOpen2, setIsOpen2] = useState(true);
+
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen2((open) => !open)}
+      >
+        {isOpen2 ? "–" : "+"}
+      </button>
+      {isOpen2 && (
+        <div className="details">
+          <header>
+            <img
+              src={selectedAnime.image}
+              alt={`${selectedAnime.title} cover`}
+            />
+            <div className="details-overview">
+              <h2>{selectedAnime.title}</h2>
               <p>
-                <em>{selectedAnime.synopsis}</em>
+                {selectedAnime.year} &bull; {selectedAnime.score}
               </p>
-            </section>
-          </div>
-        )}
-      </div>
-    </main>
+            </div>
+          </header>
+          <section>
+            <p>
+              <em>{selectedAnime.synopsis}</em>
+            </p>
+          </section>
+        </div>
+      )}
+    </div>
   );
 }
